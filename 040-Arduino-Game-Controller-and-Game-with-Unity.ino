3792b1f746c6d7741926f7f58c94cd986cce6009
monik9819 <68139660+monik9819@users.noreply.github.com>
2022-11-23 11:10:07 +0530

    Add files via upload

---
+++ 040-Arduino-Game-Controller-and-Game-with-Unity.ino
@@ -0,0 +1,17 @@
+int carRotation=0;          //variable to read the values from the potentiometers
+int carSpeed=0;
+
+void setup() {
+  Serial.begin(9600);       //start the serial communication
+  pinMode(A0,INPUT);        //initialize the potentiometers inputs
+  pinMode(A1,INPUT);
+}
+
+void loop() {
+  carRotation = analogRead(A0); //read the values of the potentiometers - between 0 and 1023
+  carSpeed = analogRead(A1);
+  Serial.print(map(carSpeed,0,1023,-100,100)); //scale it to use it with the car speed
+  Serial.print(",");
+  Serial.println(map(carRotation,0,1023,-100,100)); //scale it for the car rotation
+  delay(50);
+}
